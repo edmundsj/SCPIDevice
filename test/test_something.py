7cd73b2e4c005e741ef9819ae6ab0dd136364ef5
edmundsj <jordan.e@berkeley.edu>
2021-03-05 12:31:00 -0800

    updated test_something with pytest framework

--- test/test_something.py
+++ test/test_something.py
@@ -4,26 +4,5 @@ import numpy as np
 from numpy.testing import assert_equal, assert_allclose
 import some_module
 
-class TestSomething(unittest.TestCase):
-    def setUp(self):
-        pass # Set up to be run before every test case
-
-    @classmethod
-    def setUpClass(cls):
-        pass # Set up to be run once at the beginning 
-
-    def testSomething(self):
-        """
-        Tests that something is OK.
-        """
-        some_class = some_module.SomeClass()
-        actual_number = 1.0
-        desired_number = some_class.something
-        assert_equal(actual_number, desired_number)
-
-    def tearDown(self):
-        pass # Tear down to be run after every test case
-
-    @classmethod
-    def tearDownClass(self):
-        pass # Tear down to be run after entire script
+def test_something():
+    assert True
